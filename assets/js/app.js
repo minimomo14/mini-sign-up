685b400329cbcbb235d92cd1a435565cde91ef89
minimomo14 <93943633+minimomo14@users.noreply.github>
2023-11-02 18:48:16 -0400

    fixed bug in styles.css, create function for submit-btn in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-console.log("Hello, I'm the App!");
+document.addEventListener("DOMContentLoaded", function() {
+
+    let submit_btn = document.querySelector("#submit-btn")
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+
+    let error = document.querySelector("#error")
+
+    if(password != confirmPassword) {
+        alert("Password did not match!")
+    }
+    console.log("Hello, I'm the App!");
+
+})
