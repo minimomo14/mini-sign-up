9a620ecba31fc543859f86a367cb87d9841923cc
minimomo14 <93943633+minimomo14@users.noreply.github>
2023-11-03 18:21:19 -0400

    add media queries for mobile, and cleaning up some comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,30 +7,28 @@
 // }
 
 function validateForm() {
-  const submitButton = document.getElementById("#button");
-  const form = document.querySelector("form");
+ 
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
   const password = document.getElementById("password").value;
   const confirm_password = document.getElementById("confirmPassword").value;
-  const error = document.querySelector("#error").value;
+
 
   console.log("you see me here?");
   //first check empty first name
   if (firstName == "") {
     document.getElementById("blankMsg").innerHTML =
       "First name can not be empty.";
-
     return false;
   }
   console.log("first name");
+
   //character data validation
   if (!isNaN(firstName)) {
     document.getElementById("blankMsg").innerHTML =
       "Please enter your first name here!";
     return false;
   }
-  
 
   if (lastName == "") {
     document.getElementById("charMsg").innerHTML =
@@ -47,23 +45,33 @@ function validateForm() {
 
   //check empty password field
   if (password == "") {
-    alert("Please enter your password")
-    // document.getElementById("password").innerHTML = "Please enter the password";
+    // alert("Please enter your password")
+    document.getElementById("message1").innerHTML = "Please enter the password";
     return false;
   }
 
   //then check the confirm password field
   if (confirm_password == "") {
-    alert("Please confirm your password")
+    // alert("Please confirm your password")
+    document.getElementById("message2").innerHTML = "Please confirm your password";
     return false;
   }
 
   //set minimum password length validation
   if (password.length < 8) {
-    document.getElementById("#error").innerHTML =
+    document.getElementById("message1").innerHTML =
       "Password must be at least 8 characters";
       return false;
   }
+
+  if(password != confirm_password) {  
+    document.getElementById("message2").innerHTML = "**Passwords are not match!";  
+    return false;  
+  } else {  
+    alert ("Your password created successfully");  
+    document.write("Validation form has been submitted successfully");  
+  }  
+
 
 }
 
