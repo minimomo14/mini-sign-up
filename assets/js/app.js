3320f0b0b40ae7799a378758d550ed9e8feaf3fb
minimomo14 <93943633+minimomo14@users.noreply.github>
2023-11-02 20:40:44 -0400

    still working on functioning the validation form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+const submitButton = document.getElementById("#button");
+const form = document.querySelector("form")
+const password = document.getElementById("#password");
+const confirm_password = document.getElementById("#confirmPassword");
+const error = document.querySelector("#error")
 
-    let submit_btn = document.querySelector("#submit-btn")
-    let password = document.querySelector("#password").value;
-    let confirmPassword = document.querySelector("#confirmPassword").value;
 
-    let error = document.querySelector("#error")
-
-    if(password != confirmPassword) {
-        alert("Password did not match!")
+password.addEventListener("input", (e) => {
+    if (password != confirm_password) {
+        error.textContent = "";
+        error.className = "ERROR: Password did not match" // Password did not match!
+    } else {
+        showError();
     }
-    console.log("Hello, I'm the App!");
+});
 
-})
+form.addEventListener("submit", (e) => {
+    if(password.validity.valid) {
+        showError();
+        e.preventDefault();
+    }
+});
+
+function showError() {
+    if (password.validity.valueMissing) {
+        //if it's empty, display "You need to enter password."
+        error.textContent = "You need to enter password.";
+    } else if (password.validity.typeMismatch){
+        //check if password did not match, show error: "Password did not match"
+        error.textContent = "Please enter matching password."
+    }
+
+    error.className = "error active";
+}
